ac8d0e6ca1ba3001b5f194e3c393e7f34d0620b3
Parshvi06 <true.living1106@gmail.com>
2024-07-28 23:01:28 +0530

    react

---
+++ src/app/aboutus/page.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div>
+         <h1 className='text-center text-6xl font-bold' >About Us</h1>
+         <div className="px-16 py-16 bg-gray-300 mt-8">
+         <div className="grid grid-cols-12 mx-w-[90%]  lg-max-w-[80%] mx-auto gap-5">
+         <div className="col-span-12 md:col-span-6">
+            <h1 className="text-3xl font-bold my-3">Our Mission</h1>
+            </div>
+
+            <div className=" col-span-4 md:col-span-2">
+            <div className="color-white-500 block mb-10 text-1.1em">
+                
+                <p className='text-xl  text-bolder pt-7 pb-7'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum provident ratione beatae praesentium eius ipsa dignissimos. Sint natus dolor deleniti voluptas repellat officia sequi, voluptate quae assumenda consequatur, sed voluptatem!</p>
+            </div>
+
+        </div>
+
+
+            
+         </div>
+         </div>
+
+         <div className="px-16 py-16 bg-gray-300 mt-8">
+         <div className="grid grid-cols-12 mx-w-[90%]  lg-max-w-[80%] mx-auto gap-5">
+         <div className="col-span-12 md:col-span-6">
+            <h1 className="text-3xl  font-bold my-3">Our Vision</h1>
+            </div>
+
+            <div className=" col-span-4 md:col-span-2">
+            <div className="color-white-500 block mb-10 text-1.1em">
+                
+                <p className='text-xl  text-bolder pt-7 pb-7'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum provident ratione beatae praesentium eius ipsa dignissimos. Sint natus dolor deleniti voluptas repellat officia sequi, voluptate quae assumenda consequatur, sed voluptatem!</p>
+            </div>
+
+        </div>
+
+        
+            
+         </div>
+         </div>
+
+    </div>
+  )
+}
+
+export default AboutUs;
---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 style={{color:'blue', textAlign:'center',fontSize:50 }}>My Home Page</h1>
+    </div>
+  )
+}
+
+export default Home;
+
