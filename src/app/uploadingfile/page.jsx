702b7042114380e7c5a398194180865e52ce784e
Parshvi06 <true.living1106@gmail.com>
2024-09-03 20:26:56 +0530

    form

--- src/app/uploadingfile/page.jsx
+++ src/app/uploadingfile/page.jsx
@@ -1,11 +1,40 @@
+'use client'
+import axios from 'axios';
 import React from 'react'
+import toast from 'react-hot-toast';
 
 const uploadingFile = () => {
+  
+  const handleUpload=(e)=>{
+    const file=e.target.files[0];
+
+    const formData=new FormData();
+    formData.append('file',file)
+    formData.append('upload_preset','mypreset');
+    formData.append('cloud_name','dmnl8ozex');
+
+    axios.post('https://api.cloudinary.com/v1_1/dmnl8ozex/image/upload',formData,{
+      headers:{'Content-Type':'multipart/form-Data'}
+    })
+    .then((result) => {
+      console.log(result.data);
+      toast.success('File Uploaded Successfully');
+      
+      
+    }).catch((err) => {
+      console.log(err);
+      toast.error('Failed to Upload File');
+      
+      
+    });
+
+  }
+
   return (
     <div className='max-w-2xl mx-auto rounded-lg border-4 border-blue-300 flex justify-center p-8 mt-5'>
              <label htmlFor="uploadfile" 
              className='text-5xl font-bold text-blue-300'>⚡ Upload Your File</label>
-             <input id='uploadfile' type="file" className='hidden' />
+             <input onChange={handleUpload} id='uploadfile' type="file" className='hidden' />
     </div>
   )
 }
